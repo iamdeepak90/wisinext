3623758b0605a9c9493d2f1ba2fe63995e32cbd3
Deepak Mishra <deepak@wisitech.com>
2023-03-10 11:18:56 +0530

    WebP Update

--- pages/index.js
+++ pages/index.js
@@ -32,19 +32,19 @@ export default function Home() {
           <div className="expt_desc">You want your visitors to be your buyers and your buyers to stay with you. Our UX/UI helps you achieve that. We create interactive, user-friendly and appealing designs that speak to your customers and help generate sales!</div>
           </div>{/* expt_col */}
           <div className="expt_col">
-          <div className="expt_img"><WebPimg src="images/Web-Solutions-icon.png" alt="Web Solutions" /></div>
+          <div className="expt_img"><WebPimg src="images/Web-Solutions-icon.png" webpsrc="images/webp/Web-Solutions-icon.webp" alt="Web Solutions" /></div>
           <div className="expt_label">CUSTOMIZED</div>
           <div className="expt_title"><a href="#">Web Solutions</a></div>
           <div className="expt_desc">Build your brand, engage customers, generate sales. Get result-oriented web solutions fully customized to meet your needs. We build your scalable, robust websites and web applications on the latest technologies and frameworks.</div>
           </div>{/* expt_col */}
           <div className="expt_col">
-          <div className="expt_img"><WebPimg src="images/Graphic-Design-icon.png" alt="Graphic Design" /></div>
+          <div className="expt_img"><WebPimg src="images/Graphic-Design-icon.png" webpsrc="images/webp/Graphic-Design-icon.webp" alt="Graphic Design" /></div>
           <div className="expt_label">captivating</div>
           <div className="expt_title"><a href="#">Graphic Design</a></div>
           <div className="expt_desc">Get sharp, convincing, no-frills designs for your websites, ecommerce stores, Apps, Amazon, Etsy, eBay stores, Social Media advertisements/banners and more… Our designs speak to your customers and help generate sales!</div>
           </div>{/* expt_col */}
           <div className="expt_col">
-          <div className="expt_img"><WebPimg src="images/Mobile-Apps-icon.png" alt="Mobile Apps" /></div>
+          <div className="expt_img"><WebPimg src="images/Mobile-Apps-icon.png" webpsrc="images/webp/Mobile-Apps-icon.webp" alt="Mobile Apps" /></div>
           <div className="expt_label">NewGen</div>
           <div className="expt_title"><a href="#">Mobile Apps</a></div>
           <div className="expt_desc">Unlock the potential of your business, increase sales and enhance branding. Target and efficiently interact with your users. Build a long-lasting relationship with your customers.</div>
@@ -58,28 +58,28 @@ export default function Home() {
       <div className="whyus_abt_section">
           <div className="whyus_abt_col">
           <div className="whyus_abt_row">
-              <WebPimg src="images/year-icon.png" alt="" />
+              <WebPimg src="images/year-icon.png" webpsrc="images/webp/year-icon.webp" alt="" />
               <div className="whyus_title poppins">1999</div>
               <div className="whyus_txt">Established</div>
           </div>{/* whyus_abt_row */}
           </div>{/* whyus_abt_col */}
           <div className="whyus_abt_col">
           <div className="whyus_abt_row">
-              <WebPimg src="images/solution-icon.png" alt="" />
+              <WebPimg src="images/solution-icon.png" webpsrc="images/wepb/solution-icon.webp" alt="" />
               <div className="whyus_title poppins">24+ Years</div>
               <div className="whyus_txt">Providing Solutions</div>
           </div>{/* whyus_abt_row */}
           </div>{/* whyus_abt_col */}
           <div className="whyus_abt_col">
           <div className="whyus_abt_row">
-              <WebPimg src="images/client-icon.png" alt="" />
+              <WebPimg src="images/client-icon.png" webpsrc="images/webp/client-icon.webp" alt="" />
               <div className="whyus_title poppins">92%</div>
               <div className="whyus_txt">Repeat Clients</div>
           </div>{/* whyus_abt_row */}
           </div>{/* whyus_abt_col */}
           <div className="whyus_abt_col">
           <div className="whyus_abt_row">
-              <WebPimg src="images/worldwide-icon.png" alt="" />
+              <WebPimg src="images/worldwide-icon.png" webpsrc="images/webp/worldwide-icon.webp" alt="" />
               <div className="whyus_title poppins">1,000+</div>
               <div className="whyus_txt">Clients Worldwide</div>
           </div>{/* whyus_abt_row */}
@@ -88,28 +88,28 @@ export default function Home() {
       <div className="whyus_client">
           <div className="whyus_clnt_col">
           <div className="whyus_clnt_row">
-              <WebPimg src="images/guarantee-icon.png" />
+              <WebPimg src="images/guarantee-icon.png" webpsrc="images/webp/guarantee-icon.webp" />
               <div className="whyus_clnt_title poppins">Satisfaction Guaranteed</div>
               <div className="whyus_clnt_desc">Quality solutions within budget</div>
           </div>{/* whyus_clnt_row */}
           </div>{/* whyus_clnt_col */}
           <div className="whyus_clnt_col">
           <div className="whyus_clnt_row">
-              <WebPimg src="images/meeting-icon.png" />
+              <WebPimg src="images/meeting-icon.png" webpsrc="images/webp/meeting-icon.webp" />
               <div className="whyus_clnt_title poppins">Meeting Deadlines</div>
               <div className="whyus_clnt_desc">No delays, no cost over-runs</div>
           </div>{/* whyus_clnt_row */}
           </div>{/* whyus_clnt_col */}
           <div className="whyus_clnt_col">
           <div className="whyus_clnt_row">
-              <WebPimg src="images/confid-icon.png" />
+              <WebPimg src="images/confid-icon.png" webpsrc="images/webp/confid-icon.webp" />
               <div className="whyus_clnt_title poppins">Non-Disclosure Agreement</div>
               <div className="whyus_clnt_desc">Your confidentiality is safe with us</div>
           </div>{/* whyus_clnt_row */}
           </div>{/* whyus_clnt_col */}
           <div className="whyus_clnt_col">
           <div className="whyus_clnt_row">
-              <WebPimg src="images/presence-icon.png" />
+              <WebPimg src="images/presence-icon.png" webpsrc="images/webp/presence-icon.webp" />
               <div className="whyus_clnt_title poppins">Worldwide Presence</div>
               <div className="whyus_clnt_desc">In USA, Canada and India</div>
           </div>{/* whyus_clnt_row */}
@@ -124,42 +124,42 @@ export default function Home() {
           <div className="work_proc_wrap">
               <div className="work_proc_col">
               <div className="work_proc_row">
-                  <WebPimg src="images/goals-icon.png" alt="" />
+                  <WebPimg src="images/goals-icon.png" webpsrc="images/webp/goals-icon.webp" alt="" />
                   <div className="wrkprc_title poppins">Understand Your <br />
                   Business Goals</div>
               </div>{/* work_proc_row */}
               </div>{/* work_proc_col */}
               <div className="work_proc_col">
               <div className="work_proc_row">
-                  <WebPimg src="images/improvment-icon.png" alt="" />
+                  <WebPimg src="images/improvment-icon.png" webpsrc="images/webp/improvment-icon.webp" alt="" />
                   <div className="wrkprc_title poppins">Identify Areas <br />
                   of Improvement</div>
               </div>{/* work_proc_row */}
               </div>{/* work_proc_col */}
               <div className="work_proc_col">
               <div className="work_proc_row">
-                  <WebPimg src="images/mark-need-icon.png" alt="" />
+                  <WebPimg src="images/mark-need-icon.png" webpsrc="images/webp/mark-need-icon.webp" alt="" />
                   <div className="wrkprc_title poppins">Map Out the Technology <br />
                   &amp; Marketing Needs</div>
               </div>{/* work_proc_row */}
               </div>{/* work_proc_col */}
               <div className="work_proc_col">
               <div className="work_proc_row">
-                  <WebPimg src="images/appr-solution-icon.png" alt="" />
+                  <WebPimg src="images/appr-solution-icon.png" webpsrc="images/webp/appr-solution-icon.webp" alt="" />
                   <div className="wrkprc_title poppins">Consult &amp; Advise <br />
                   the Appropriate Solutions</div>
               </div>{/* work_proc_row */}
               </div>{/* work_proc_col */}
               <div className="work_proc_col">
               <div className="work_proc_row">
-                  <WebPimg src="images/roadmap-icon.png" alt="" />
+                  <WebPimg src="images/roadmap-icon.png" webpsrc="images/webp/roadmap-icon.webp" alt="" />
                   <div className="wrkprc_title poppins">Plan &amp; Implement <br />
                   Solutions Roadmap</div>
               </div>{/* work_proc_row */}
               </div>{/* work_proc_col */}
               <div className="work_proc_col">
               <div className="work_proc_row">
-                  <WebPimg src="images/success-icon.png" alt="" />
+                  <WebPimg src="images/success-icon.png" webpsrc="images/webp/success-icon.webp" alt="" />
                   <div className="wrkprc_title poppins">Provide Support <br />
                   to Ensure Success</div>
               </div>{/* work_proc_row */}
@@ -184,7 +184,7 @@ export default function Home() {
           </div>{/* tstmnl_desc */}
           </div>{/* tstmnl_lft */}
           <div className="tstmnl_rgt">
-          <WebPimg src="images/tess.png" />
+          <WebPimg src="images/tess.png" webpsrc="images/webp/tess.webp" />
           </div>{/* tstmnl_rgt */}
       </div>{/* tstmnl_wrap */}
       <div className="tstmnl_more"><a href="#">More Testimonials</a></div>
@@ -196,21 +196,21 @@ export default function Home() {
           <div className="ourclnt_label poppins">Trusted By The</div>
           <div className="heading poppins pad0">World’s Best Companies</div>
           <div className="ourclnt_row">
-          <WebPimg src="images/client-logo-1.png" alt="" />
-          <WebPimg src="images/client-logo-2.png" alt="" />
-          <WebPimg src="images/client-logo-3.png" alt="" />
-          <WebPimg src="images/client-logo-4.png" alt="" />
-          <WebPimg src="images/client-logo-5.png" alt="" />
-          <WebPimg src="images/client-logo-6.png" alt="" />
-          <WebPimg src="images/client-logo-7.png" alt="" />
-          <WebPimg src="images/client-logo-8.png" alt="" />
+          <WebPimg src="images/client-logo-1.png" webpsrc="images/webp/client-logo-1.webp" alt="" />
+          <WebPimg src="images/client-logo-2.png" webpsrc="images/webp/client-logo-2.webp" alt="" />
+          <WebPimg src="images/client-logo-3.png" webpsrc="images/webp/client-logo-3.webp" alt="" />
+          <WebPimg src="images/client-logo-4.png" webpsrc="images/webp/client-logo-4.webp" alt="" />
+          <WebPimg src="images/client-logo-5.png" webpsrc="images/webp/client-logo-5.webp" alt="" />
+          <WebPimg src="images/client-logo-6.png" webpsrc="images/webp/client-logo-6.webp" alt="" />
+          <WebPimg src="images/client-logo-7.png" webpsrc="images/webp/client-logo-7.webp" alt="" />
+          <WebPimg src="images/client-logo-8.png" webpsrc="images/webp/client-logo-8.webp" alt="" />
           </div>{/* ourclnt_row */}
       </div>{/* outer_wrapper */}
       </div>{/* our_client_section */}
       <div className="let_tlk_section">
       <div className="let_tlk_wrap">
           <div className="letlk_lft">
-          <WebPimg src="images/partho-mandal.png" websrc="images/webp/partho-mandal.webp" alt="" />
+          <WebPimg src="images/partho-mandal.png" webpsrc="images/webp/partho-mandal.webp" alt="" />
           </div>{/* letlk_lft */}
           <div className="letlk_rgt">
           <p>“When I founded Wisitech in January 1999, I believed we were at the right time to ride the crest of the digital wave as the World Wide Web exploded. We’ve stayed ahead of the technology trends to offer solutions that help our customers walk the web wisely and avoid the pitfalls. From ideation to design, from development to marketing, we partner you with a 360 degree approach. We know what works and will ensure it works for you.”</p>
@@ -223,62 +223,61 @@ export default function Home() {
       <div className="heading">Our Blog</div>
       <div className="blog_section">
           <div className="blog_col">
-          <WebPimg src="images/blog-1.png" alt="" />
+          <WebPimg src="images/blog-1.png" webpsrc="images/webp/blog-1.webp" alt="" />
           <a className="blog_title" href="#">How can you Benefit from Content Writing Services?</a>
           </div>{/* blog_col */}
           <div className="blog_col">
-          <WebPimg src="images/blog-2.png" alt="" />
+          <WebPimg src="images/blog-2.png" webpsrc="images/webp/blog-2.webp" alt="" />
           <a className="blog_title" href="#">Responsive Web Design Services  Create a Mobile-Friendly</a>
           </div>{/* blog_col */}
       </div>{/* blog_section */}
       <div className="award_section">
           <div className="heading">Recognitions &amp; Awards</div>
           <div className="award_row">
-          <div className="award_img"><WebPimg src="images/award-1.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-2.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-3.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-4.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-5.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-6.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-7.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-8.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-9.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-10.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-11.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-12.png" alt="" /></div>				
-          <div className="award_img"><WebPimg src="images/award-14.png" alt="" /></div>
-          <div className="award_img"><WebPimg src="images/award-13.png" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-1.png" webpsrc="images/webp/award-1.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-2.png" webpsrc="images/webp/award-2.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-3.png" webpsrc="images/webp/award-3.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-4.png" webpsrc="images/webp/award-4.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-5.png" webpsrc="images/webp/award-5.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-6.png" webpsrc="images/webp/award-6.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-7.png" webpsrc="images/webp/award-7.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-8.png" webpsrc="images/webp/award-8.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-9.png" webpsrc="images/webp/award-9.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-10.png" webpsrc="images/webp/award-10.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-11.png" webpsrc="images/webp/award-11.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-12.png" webpsrc="images/webp/award-12.webp" alt="" /></div>
+          <div className="award_img"><WebPimg src="images/award-13.png" webpsrc="images/webp/award-13.webp" alt="" /></div>
           </div>{/* award_row */}
       </div>{/* award_section */}
       <div className="google_ads_section">
           <div className="heading padT0">Google Ads Certified</div>
           <div className="google_ads_row">
           <div className="google_ads_col">
-              <WebPimg src="images/google-ads-1.png" alt="" />
+              <WebPimg src="images/google-ads-1.png" webpsrc="images/webp/google-ads-1.webp" alt="" />
               <div className="google_ads_txt">Google<br />
               Ads Video<br />
               Certification</div>
           </div>{/* google_ads_col */}
           <div className="google_ads_col">
-              <WebPimg src="images/google-ads-2.png" alt="" />
+              <WebPimg src="images/google-ads-2.png" webpsrc="images/webp/google-ads-2.webp" alt="" />
               <div className="google_ads_txt">Google<br />
               Campaign Manager<br />
               Certification</div>
           </div>{/* google_ads_col */}
           <div className="google_ads_col">
-              <WebPimg src="images/google-ads-3.png" alt="" />
+              <WebPimg src="images/google-ads-3.png" webpsrc="images/webp/google-ads-3.webp" alt="" />
               <div className="google_ads_txt">Google<br />
               Ads Search<br />
               Certification</div>
           </div>{/* google_ads_col */}
           <div className="google_ads_col">
-              <WebPimg src="images/google-ads-4.png" alt="" />
+              <WebPimg src="images/google-ads-4.png" webpsrc="images/webp/google-ads-4.webp" alt="" />
               <div className="google_ads_txt">Google<br />
               Ads Display<br />
               Certification</div>
           </div>{/* google_ads_col */}
           <div className="google_ads_col">
-              <WebPimg src="images/google-ads-5.png" alt="" />
+              <WebPimg src="images/google-ads-5.png" webpsrc="images/webp/google-ads-5.webp" alt="" />
               <div className="google_ads_txt">Google<br />
               Shopping Ads<br />
               Certification</div>
